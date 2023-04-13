6ee742f3e5be0fe7fdbbe1dcd38998c421256125
katzhop <kaitnich22@gmail.com>
2023-04-13 13:26:21 -0400

    Adding Pages and some layouts

---
+++ app/src/main/java/com/example/d2l_msdproject/ui/appMainFragment.kt
@@ -0,0 +1,67 @@
+package com.example.d2l_msdproject.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.example.d2l_msdproject.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class appMainFragment : Fragment() {
+
+    //private val viewModel: appDashFragment by viewModels()
+    private lateinit var binding: FragmentAppMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //binding set up
+        binding= FragmentAppMainBinding.inflate(inflater, container, false)
+
+        //viewpager adapter setup
+        val viewPager: ViewPager2 = binding.viewPager2
+        val bottomNavigationView: BottomNavigationView = binding.bottomNav
+
+        //attach fragment activity to adapter
+        val activity = requireActivity()
+        val viewPagerAdapter = ViewPagerAdapter(activity)
+        viewPager.adapter = viewPagerAdapter
+
+        //bottom nav on click
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.listBottomNav -> viewPager.currentItem = 0
+                R.id.groupBottomNav -> viewPager.currentItem = 1
+                R.id.friendsBottomNav -> viewPager.currentItem = 2
+                R.id.profileBottomNav -> viewPager.currentItem = 3
+            }
+            true
+        }
+
+
+
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
+
